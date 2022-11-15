d91c0ff68a26fabd8f2a5ed32f9a825c96c83e82
MJ-Curtido <manu17pc@gmail.com>
2022-11-15 14:41:42 +0100

    Ejercicio 5

--- DEINT-EjerciciosFicheros/Program.cs
+++ DEINT-EjerciciosFicheros/Program.cs
@@ -1,12 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Text.RegularExpressions;
 
-String ruta1 = "C:\\Users\\Dam\\Downloads\\texto1.txt";
-String ruta2 = "C:\\Users\\Dam\\Downloads\\texto2.txt";
+//C:\Users\Dam\Downloads\prueba\texto1.txt
+
+String ruta1 = "";
+String ruta2 = "";
 String texto = "";
 String frase = "";
 String[] lineas;
 
+/*
 Console.WriteLine("-------1-------");
 Console.WriteLine("Introduzca una ruta");
 ruta1 = Console.ReadLine();
@@ -16,6 +19,8 @@ ruta2 = Console.ReadLine();
 
 texto = File.ReadAllText(ruta1);
 File.WriteAllText(ruta2, texto);
+
+Console.WriteLine("Operación realizada.");
 
 
 Console.WriteLine("\n\n-------2-------");
@@ -28,7 +33,7 @@ frase = Console.ReadLine();
 lineas = File.ReadAllLines(ruta1);
 for (int i = 0; i < lineas.Length; i++)
 {
-	if (Regex.IsMatch(lineas[i], @"^(.*\s" + frase + @"\s.*)$ || ^(" + frase + @"\s.*)$ || ^(.*\s" + frase + @")$ || ^(\s*" + frase + @"\s*)$"))
+	if (lineas[i].Contains(frase))
 	{
 		Console.WriteLine((i + 1) + ". " + lineas[i]);
 	}
@@ -43,3 +48,39 @@ Console.WriteLine("Introduzca otra ruta");
 ruta2 = Console.ReadLine();
 
 File.Copy(ruta1, ruta2, true);
+
+Console.WriteLine("Operación realizada.");
+
+
+Console.WriteLine("\n\n-------4-------");
+String rutaNotas = "C:\\Users\\Dam\\Downloads\\prueba\\rutaNotas.txt";
+String rutaSolucion = "C:\\Users\\Dam\\Downloads\\prueba\\rutaSolucion.txt";
+String[] celdas;
+String nomAlumno = "";
+Double sumaNotas = 0.0;
+int cont = 0;
+Double maxNota = 0.0;
+
+lineas = File.ReadAllLines(rutaNotas);
+for (int i = 0; i < lineas.Length; i++)
+{
+	celdas = lineas[i].Split("-");
+
+	sumaNotas += Double.Parse(celdas[1]);
+	cont++;
+
+	if (Double.Parse(celdas[2]) > maxNota)
+	{
+		maxNota = Double.Parse(celdas[2]);
+		nomAlumno = celdas[0];
+	}
+}
+
+texto = $"Nota media de matemáticas: {Math.Round(sumaNotas / cont * 100) / 100}\nAlumno con más nota: {nomAlumno} -> {maxNota}";
+
+File.WriteAllText(rutaSolucion, texto);
+
+Console.WriteLine("Operación realizada.");
+*/
+
+
